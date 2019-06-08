99d78101e95bf85d8dd843fd36a56c1ac3045538
uplink42 <diamantinorgf@gmail.com>
2019-06-08 14:51:56 +0100

    assets setup

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [NavComponent],
     imports: [
         CommonModule,
     ],
